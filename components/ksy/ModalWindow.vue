87779277f828702e473caf79093815733c623ea1
Ksenia Alysheva <ksyusha.alysheva@gmail.com>
2025-06-23 17:01:58 +0300

    Fix_modal

--- components/ksy/ModalWindow.vue
+++ components/ksy/ModalWindow.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ButtonVue from "~/components/ksy/ButtonVue.vue";
+import {reactive, ref} from "vue";
 
 defineProps({
   open:{
@@ -8,9 +9,128 @@ defineProps({
   }
 })
 
-const emit = defineEmits(['close'])
+const emit = defineEmits(['close']);
 function close(){
   emit('close', false);
+}
+
+const errors = reactive({
+  errorName: false,
+  errorPhone: false,
+  errorEmail: false,
+})
+
+const formModal = reactive({
+  name: '',
+  phone: '',
+  email: ''
+})
+
+const errorName = reactive({
+  name: '',
+  phone: '',
+  email: ''
+})
+
+function checkName(){
+  let reg = /^[a-zA-Zа-яА-Я]{3,25}$/;
+  if(formModal.name === ''){
+    errors.errorName = true;
+    errorName.name = 'Это обязательное поле для заполнения';
+  } else {
+    if(reg.test(formModal.name)){
+      errors.errorName = false;
+    } else {
+      errors.errorName = true;
+      errorName.name = 'Только буквы (3-25 символов)';
+    }
+  }
+}
+function checkPhone(){
+  let reg = /^(\+7|8)\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+  if(formModal.phone === ''){
+    errors.errorPhone = true;
+    errorName.phone = 'Это обязательное поле для заполнения';
+  } else {
+    if(reg.test(formModal.phone)){
+      errors.errorPhone = false;
+    } else {
+      errors.errorPhone = true;
+      errorName.phone = '8(+7)(###)###-##-##';
+    }
+  }
+}
+function checkEmail(){
+  let reg = /^[a-zA-Z0-9]+@(mail.ru|gmail.com|yandex.ru)$/;
+  if(formModal.email === ''){
+    errors.errorEmail = true;
+    errorName.email = 'Это обязательное поле для заполнения';
+  } else {
+    if(reg.test(formModal.email)){
+      errors.errorEmail = false;
+    } else {
+      errors.errorEmail = true;
+      errorName.email = 'Некорректный email';
+    }
+  }
+}
+
+const submite = ref(false);
+function send(){
+  let regName = /^[a-zA-Zа-яА-Я]{3,25}$/;
+  let regPhone = /^(\+7|8)\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+  let regEmail = /^[a-zA-Z0-9]+@(mail.ru|gmail.com|yandex.ru)$/;
+  let error = false;
+  if(formModal.name === ''){
+    errors.errorName = true;
+    errorName.name = 'Это обязательное поле для заполнения'
+    error = true
+  } else {
+    if(regName.test(formModal.name)){
+      errors.errorName = false;
+      error = false
+    } else {
+      errors.errorName = true;
+      errorName.name = 'Только буквы (3-25 символов)';
+      error = true
+    }
+  }
+  if(formModal.phone === ''){
+    errors.errorPhone = true;
+    errorName.phone = 'Это обязательное поле для заполнения';
+    error = true
+  } else {
+    if(regPhone.test(formModal.phone)){
+      errors.errorPhone = false;
+      error = false
+    } else {
+      errors.errorPhone = true;
+      errorName.phone = '8(+7)(###)###-##-##';
+      error = true
+    }
+  }
+  if(formModal.email === ''){
+    errors.errorEmail = true;
+    errorName.email = 'Это обязательное поле для заполнения';
+    error = true
+  } else {
+    if(regEmail.test(formModal.email)){
+      errors.errorEmail = false;
+      error = false
+    } else {
+      errors.errorEmail = true;
+      errorName.email = 'Некорректный email';
+      error = true
+    }
+  }
+  if (!error){
+    submite.value = true;
+    setTimeout(() => emit('close', false), 1500);
+    setTimeout(() => submite.value = false, 1500);
+    formModal.name = '';
+    formModal.phone = '';
+    formModal.email = '';
+  }
 }
 </script>
 
@@ -20,7 +140,10 @@ function close(){
         crossorigin="anonymous" referrerpolicy="no-referrer"/>
 
   <div class="background" v-if="open">
-    <div class="content">
+    <div class="successMessage" v-if="submite">
+      Форма успешно отправлена! Мы свяжемся с вами в ближайшее время.
+    </div>
+    <form class="content" v-if="!submite">
       <div class="head">
         <img src="/img/logo.svg">
         <i class="fa-solid fa-xmark" @click="close"></i>
@@ -28,9 +151,21 @@ function close(){
       <div class="body">
         <h1>Оставить заявку</h1>
         <div class="inputs">
-          <input placeholder="ФИО *">
-          <input placeholder="Телефон *">
-          <input placeholder="Email *">
+          <div class="input">
+            <input type="text" placeholder="ФИО *" v-model="formModal.name" @focusout="checkName"
+                   :class="{'error': errors.errorName}">
+            <span v-if="errors.errorName">{{errorName.name}}</span>
+          </div>
+          <div class="input">
+            <input placeholder="Телефон *" v-model="formModal.phone" @focusout="checkPhone"
+                   :class="{'error': errors.errorName}">
+            <span v-if="errors.errorPhone">{{errorName.phone}}</span>
+          </div>
+          <div class="input">
+            <input placeholder="Email *" v-model="formModal.email" @focusout="checkEmail"
+                   :class="{'error': errors.errorName}">
+            <span v-if="errors.errorEmail">{{errorName.email}}</span>
+          </div>
           <input class="message" placeholder="Комментарий">
         </div>
         <div class="foot">
@@ -39,10 +174,10 @@ function close(){
             <label for="checkbox">Даю согласие на обработку персональных данных и подтверждаю, что ознакомлен с
               политикой обработки персональных данных</label>
           </div>
-          <button-vue class="button-vue"/>
+          <button class="button-vue" @click="send" type="button">Отправить ></button>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -50,6 +185,17 @@ function close(){
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css";
 *{
   font-family: Monserrat;
+}
+
+button{
+  border: none;
+  background-color: #880003;
+  color: white;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 8px 122px;
+  border-radius: 5px;
+  width: 348px;
 }
 
 .head{
@@ -66,7 +212,35 @@ function close(){
   border-radius: 2px;
   padding: 3px 5px;
   height: fit-content;
+  font-size: 18px;
 }
+
+.successMessage{
+  background-color: #fff;
+  border-radius: 20px;
+  width: 100%;
+  border: 2px solid #880003;
+  padding: 70px;
+  font-size: 20px;
+  color: #880003;
+  font-weight: 700;
+  text-align: center;
+}
+.error{
+  border: 2px solid #D51317;
+}
+
+span{
+  color: #D51317;
+  margin-top: 5px;
+  font-size: 12px;
+}
+.input{
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
 .background{
   top: 0;
   bottom: 0;
@@ -97,7 +271,6 @@ input{
   font-size: 20px;
   font-weight: 400;
   border: 2px solid #545454;
-  margin-bottom: 10px;
   border-radius: 5px;
   color: #333333;
 }
@@ -106,6 +279,7 @@ input{
   flex-direction: column;
   margin-left: 30px;
   margin-right: 30px;
+  margin-bottom: 15px;
 }
 .message{
   height: 140px;
@@ -168,7 +342,6 @@ input[type="checkbox"]:checked::after{
   font-size: 16px;
   color: #333333;
   font-weight: 600;
-  transform: translateY(-5px);
   width: 80%;
 }
 .button-vue{
